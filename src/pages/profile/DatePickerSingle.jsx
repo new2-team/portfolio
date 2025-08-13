2da8cda959e2a9e7f3da00b9fd19034b3ad6d45c
keegennie <keegennie9@gmail.com>
2025-08-13 19:23:02 +0900

    프로필 수정 7

--- src/pages/profile/DatePickerSingle.jsx
+++ src/pages/profile/DatePickerSingle.jsx
@@ -1,146 +1,116 @@
-import React, { forwardRef, useState } from 'react';
-import DatePicker from 'react-datepicker';
+import React, { forwardRef, useImperativeHandle, useState } from "react";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import styled from 'styled-components';
-import BasicInput from '../../components/input/BasicInput';
-import { getMonth, getYear } from 'date-fns';
-
+import styled from "styled-components";
+import { getMonth, getYear } from "date-fns";
 
 const StyledDatePicker = styled.div`
   .react-datepicker {
-    width: 350px ;
-    height: 400px ;
+    width: 350px;
+    /* height: 400px; */
     box-sizing: border-box;
   }
-  
-  /* 헤더 고정 높이 */
   .react-datepicker__header {
-    height: 80px ;
+    height: 80px;
     flex-shrink: 0;
   }
-  
-  /* 월 컨테이너 나머지 공간 차지 */
   .react-datepicker__month-container {
-    height: 320px ; /* 전체 - 헤더 높이 */
+    /* height: 320px; */
     overflow: hidden;
   }
-
-  
-  /* 각 날짜 셀 크기 고정 */
   .react-datepicker__day {
-      width: 40px ;
-      height: 40px ;
-      line-height: 40px;
-      margin: 1px 
-    }
-    
-    .react-datepicker__day-name {
-        width: 40px ;
-        height: 30px ;
-        line-height: 30px ;
-    }
-    .react-datepicker__month {
-      flex-direction: column-reverse; 
-    }
-    `;
-    const CustomInput = React.forwardRef(({ value, onClick}, ref) => {
-        return(
-            <BasicInput
-            onClick = {onClick} 
-            ref = {ref}
-            value = {value}
-            readOnly
-            style={{width: '100%'}}
-        />);
-    })
+    width: 40px;
+    height: 40px;
+    justify-content: space-between;
+    line-height: 40px;
+    /* margin: 0px; */
+  }
+
+  .react-datepicker__day-name {
+    width: 40px;
+    height: 30px;
+    line-height: 30px;
+  }
+  .react-datepicker__month {
+    flex-direction: column;
+  }
+`;
 
 const DatePickerSingle = forwardRef(({ selected, onChange, ...props }, ref) => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
-const currentYear = getYear(new Date());
-const years = Array.from(
-    { length: currentYear - 1990 + 1 }, 
-    (_, i) => 1990 + i
-);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useImperativeHandle(ref, () => ({
+    setFocus: () => setIsOpen(true),
+    setOpen: (open) => setIsOpen(open),
+  }));
+
+  const currentYear = getYear(new Date());
+  const years = Array.from({ length: currentYear - 1990 + 1 }, (_, i) => 1990 + i);
   const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    "January","February","March","April","May","June",
+    "July","August","September","October","November","December",
   ];
 
-  const handleDateChange = (date) => {
-        setSelectedDate(date);
-        if (onchange) onchange(date); // 부모 컴포넌트로 전달
-  };
-
   return (
-    <StyledDatePicker
-        ref={ref}
-        customInput={<CustomInput />}
-      renderCustomHeader={({
-        date,
-        changeYear,
-        changeMonth,
-        decreaseMonth,
-        increaseMonth,
-        prevMonthButtonDisabled,
-        nextMonthButtonDisabled,
-      }) => (
-        <div
-          style={{
-            margin: 10,
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}> 
-            {"<"}
-          </button>
-          <select
-            value={getYear(date)}
-            onChange={({ target: { value } }) => changeYear(value)}
-          >
-            {years.map((option) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
-          </select>
+    <>
+      {isOpen && (
+        <StyledDatePicker>
+          <DatePicker
+            {...props}
+            selected={selected}
+            onChange={(date) => {
+              if (onChange) onChange(date);
+              setIsOpen(false);
+            }}
+            inline // 인풋 대신 달력만 표시
+            renderCustomHeader={({
+              date,
+              changeYear,
+              changeMonth,
+              decreaseMonth,
+              increaseMonth,
+              prevMonthButtonDisabled,
+              nextMonthButtonDisabled,
+            }) => (
+              <div style={{ margin: 10, display: "flex", justifyContent: "center", gap: "4px" }}>
+                <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+                  {"<"}
+                </button>
+                <select
+                  value={getYear(date)}
+                  onChange={({ target: { value } }) => changeYear(Number(value))}
+                >
+                  {years.map((option) => (
+                    <option key={option} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
 
-          <select
-            value={months[getMonth(date)]}
-            onChange={({ target: { value } }) =>
-              changeMonth(months.indexOf(value))
-            }
-          >
-            {months.map((option) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
-          </select>
+                <select
+                  value={months[getMonth(date)]}
+                  onChange={({ target: { value } }) =>
+                    changeMonth(months.indexOf(value))
+                  }
+                >
+                  {months.map((option) => (
+                    <option key={option} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
 
-          <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
-            {">"}
-          </button>
-        </div>
+                <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+                  {">"}
+                </button>
+              </div>
+            )}
+          />
+        </StyledDatePicker>
       )}
-      selected={selectedDate}
-      setSelectedDate={setSelectedDate}
-      onChange={handleDateChange}
-      // customInput={<CustomInput />} // 팀 공용 컴포넌트를 그대로 사용
-      
-    />
-    
+    </>
   );
-  
 });
+
+
 export default DatePickerSingle;
